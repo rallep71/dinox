4c9feea9b9b7e963220c66dae28419c01a47990a
rallep71 <huepferralf@gmail.com>
2026-01-10 08:06:13 +0100

    Fix AppImage build: use --appimage-extract-and-run to avoid FUSE issues on CI

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -586,7 +586,7 @@ create_appimage() {
     
     # Create AppImage with update information and zsync
     log_info "Running appimagetool with update support..."
-    ARCH=$ARCH "$APPIMAGETOOL" \
+    ARCH=$ARCH "$APPIMAGETOOL" --appimage-extract-and-run \
         --updateinformation "$UPDATE_INFO" \
         "$APPDIR" "$APPIMAGE_NAME"
     
